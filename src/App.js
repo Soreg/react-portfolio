35f2f5633e8ef52aea4b6393bc15ed6f65d56962
Soren Gravesen <S_Gravesen92@hotmail.com>
2017-10-28 15:28:48 +0200

    added scss loader

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 
+import './styles/styles.scss';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <h1>TEST</h1>
       </div>
     );
   }
