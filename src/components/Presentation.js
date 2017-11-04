709541418215b637c6f1f09bf947dd2696d86eee
Soren Gravesen <S_Gravesen92@hotmail.com>
2017-11-04 20:18:30 +0100

    Changed text in presentation

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -10,10 +10,10 @@ const Presentation = () => {
         </div>
         <div className="col-md-10 col-xs-10 col-md-push-1 col-xs-push-1 text-left">
           <p>
-            Hello, and welcome to my wonderful world of programming! My name is Soren, and I am a passionate web developer. I love to stay up to date with new trends, and I always devote time to 'upgrade' myself. I know a variety of languages, mostly related to front-end development.
+            Hello, and welcome to my wonderful world of programming! My name is Søren, and I am a passionate web developer. I love to stay up to date with new trends, and I always devote time to 'upgrade' myself. I know a variety of languages, mostly related to front-end development.
           </p>
           <p>
-            My programming journey started around 5 years ago, when I got introduced to C# in a class I was taking. I was hooked from the first day, and I have been ever since! I quickly changed from C# to Html, Css and Javascript, and also started working with PHP and SQL. My current focus is ReactJS!
+            My programming journey started around 5 years ago, when I got introduced to C# in a class I was taking. I was hooked from the first day, and I have been ever since! I quickly changed from C# to HTML/CSS and Javascript, as well as PHP and SQL. My current focus is mastering ReactJS!
           </p>
         </div>
        </div>
